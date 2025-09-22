9306a62938810728251788fd3248ad4bfd95fb84
sanjay-2420030014 <2420030014@klh.edu.in>
2025-09-22 11:23:13 +0530

    week 7

---
+++ Week 7/data.js
@@ -0,0 +1,6 @@
+export const books = [
+  { title: "JavaScript Essentials", author: "John Doe", price: 499, availability: "in stock" },
+  { title: "Learn React", author: "Aashrith", price: 699, availability: "in stock" },
+  { title: "Node.js in Action", author: "prahul", price: 599, availability: "out of stock" },
+  { title: "Python for Everyone", author: "Ramya Krishna", price: 450, availability: "in stock" }
+];
